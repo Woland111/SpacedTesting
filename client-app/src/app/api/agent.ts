21e7f18c3f30825d94d6f91f3838dc808fbd5763
Luka <Woland111@gmail.com>
2021-12-18 13:04:26 +0100

    Added generic types to the agent file.

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -4,17 +4,17 @@ import { Skill } from '../models/skill';
 axios.defaults.baseURL = 'http://localhost:5000/api/';
 
 const httpRequests = {
-  get: (url: string) => axios.get(url).then((response) => response.data),
-  post: (url: string, body: {}) =>
-    axios.post(url, body).then((response) => response.data),
-  put: (url: string, body: {}) =>
-    axios.put(url, body).then((response) => response.data),
-  delete: (url: string) => axios.delete(url).then((response) => response.data),
+  get: <T>(url: string) => axios.get<T>(url).then((response) => response.data),
+  post: <T> (url: string, body: {}) =>
+    axios.post<T>(url, body).then((response) => response.data),
+  put: <T> (url: string, body: {}) =>
+    axios.put<T>(url, body).then((response) => response.data),
+  delete: <T> (url: string) => axios.delete<T>(url).then((response) => response.data),
 };
 
 const skillsApi = {
   create: (skill: Skill) => httpRequests.post('skills', skill),
-  readAll: () => httpRequests.get('skills'),
+  readAll: () => httpRequests.get<Skill[]>('skills'),
   update: (skill: Skill) => httpRequests.put(`skills/${skill.id}`, skill),
   delete: (id: string) => httpRequests.delete(`skills/${id}`),
 };
