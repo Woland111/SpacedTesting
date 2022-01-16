0f1461a16bf6bad7f061cf8b9a96cac61e327e79
Luka <Woland111@gmail.com>
2022-01-16 13:43:32 +0100

    Added the key property to the SkillListItem component on the SkillsList component.

--- client-app/src/features/skills/management/SkillsList.tsx
+++ client-app/src/features/skills/management/SkillsList.tsx
@@ -1,18 +1,17 @@
-import { observer } from "mobx-react-lite";
-import { Link } from "react-router-dom";
-import { Button, Item, Label, Segment } from "semantic-ui-react";
-import { useStore } from "../../../app/stores/store";
-import SkillListItem from "./SkillListItem";
+import { observer } from 'mobx-react-lite';
+import { Item, Segment } from 'semantic-ui-react';
+import { useStore } from '../../../app/stores/store';
+import SkillListItem from './SkillListItem';
 
 export default observer(function SkillsList() {
-  const { skillStore} = useStore();
+  const { skillStore } = useStore();
   return (
     <Segment>
       <Item.Group divided>
         {skillStore.skillsSortedByCreationDate.map((skill) => (
-          <SkillListItem skill={skill} />
+          <SkillListItem key={skill.id} skill={skill} />
         ))}
       </Item.Group>
     </Segment>
   );
-})
+});
